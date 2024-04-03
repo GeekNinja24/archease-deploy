cd9c8dfb32f9b08fe03ca2ce62e3d2d22f650501
Ved Nig <vednig12@gmail.com>
2024-04-03 11:11:43 +0530

    'Updated'

---
+++ src/Create.jsx
@@ -0,0 +1,12 @@
+
+const Create = () => {
+
+  
+  return (
+    <>
+     <iframe  src="https://cvdlab.github.io/react-planner/" width="100%" style={{height: '100vh'}}></iframe>
+    </>
+  );
+};
+
+export default Create;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 
 import App from "./App.jsx";
+import Create from "./Create.jsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
+    <Router basename="app" >
+        {/* <App />  */}
+        <Routes>
+          <Route path="/" element={<App />} />
+        <Route path="/create" element={<Create />  }/>
+
+        </Routes>
     </Router>
   </React.StrictMode>
 );
